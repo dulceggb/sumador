3c8952bc13221f8dea2e2d9bbdb62451180f3b7d
dulceg <dulce.ggb11@gmail.com>
2020-05-02 02:02:57 -0500

    YA lee n numeros

--- main.c
+++ main.c
@@ -7,7 +7,13 @@
 
 int main(int argc, char **argv)
 {
-	printf("Hola Mundo\n");
+	int n, arreglo[100], indice;
+	printf("Cuantos numeros va a sumar:\n");
+	scanf("%d", &n);
+	for(indice=0;indice<n;indice++){
+		printf("Cual es el numero  %d: ", indice);
+		scanf("%d", &(arreglo[indice]));
+	}
 	return 0;
 }
 
