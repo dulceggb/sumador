6627083d083178e53f20f64578de38b6468b03e5
dulceg <dulce.ggb11@gmail.com>
2020-05-02 02:28:32 -0500

    YA FUNCIONA

--- main.c
+++ main.c
@@ -8,10 +8,13 @@
 
 int main(int argc, char **argv)
 {
-	int n, arreglo[100], indice;
+	int n, arreglo[100], indice, suma=0;
 	printf("Cuantos numeros va a sumar:\n");
 	scanf("%d", &n);
 	leerArreglo(n, arreglo);
+	for(indice=0; indice<n; indice++)
+	suma+=arreglo[indice];
+	printf("La suma es: %d \n", suma);
 	return 0;
 }
 
